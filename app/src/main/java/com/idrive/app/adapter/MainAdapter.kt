d0447a5ffcfd1f9fd08d58be6f57ca70ee9f8d53
Akhtar ali <akthar.ali@embitel.com>
2022-08-22 16:36:32 +0530

    addedd progress bar in layout

--- app/src/main/java/com/idrive/app/adapter/MainAdapter.kt
+++ app/src/main/java/com/idrive/app/adapter/MainAdapter.kt
@@ -7,6 +7,9 @@ import com.idrive.app.databinding.ItemsRowBinding
 import com.idrive.app.model.TouristModel
 
 class MainAdapter(private val list: List<TouristModel>) : RecyclerView.Adapter<MainAdapter.MainViewHolder>() {
+    private var isLoading = false
+    private val VIEWTYPE_LIST = 1
+    private val VIEWTYPE_PROGRESS = 2
 
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): MainViewHolder {
         val inflater = LayoutInflater.from(parent.context)
@@ -23,6 +26,10 @@ class MainAdapter(private val list: List<TouristModel>) : RecyclerView.Adapter<M
 
     override fun getItemCount(): Int {
         return list.size
+    }
+
+    override fun getItemViewType(position: Int): Int {
+        return super.getItemViewType(position)
     }
 
     class MainViewHolder(val binding: ItemsRowBinding) : RecyclerView.ViewHolder(binding.root) {
